45735a130bb8248a5b0f32f893ab9f7c113da956
AndriiDronDiachuk <dyachuk.droid@gmail.com>
2017-06-30 13:22:38 +0300

    heroku deploy 7

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,9 @@ const Sequelize = require('sequelize');
 
 //var web_config = process.env.DATABASE_URL;
 
-const webConnection = new Sequelize(process.env.DATABASE_URL);
+const DATABASE_URL = "postgres://btrhekrtdobvqb:273bf4f1340c9a3c15afecc3c58690eda6e3bc1f29fd1a29e7ecdf949d693633@ec2-54-75-229-201.eu-west-1.compute.amazonaws.com:5432/d28q1qpcbvrn73"
+
+const webConnection = new Sequelize(DATABASE_URL);
 
 const localConnection = new Sequelize('chessdb', 'postgres', '1111', {
     host: 'localhost',
