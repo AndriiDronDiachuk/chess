802f392d82c5b8751824ed86fcd5c5ba3b03846e
AndriiDronDiachuk <dyachuk.droid@gmail.com>
2017-06-30 15:16:12 +0300

    Пофиксил статистику

--- db/connection.js
+++ db/connection.js
@@ -3,8 +3,6 @@
  */
 const Sequelize = require('sequelize');
 
-//var web_config = process.env.DATABASE_URL;
-
 const DATABASE_URL = "postgres://btrhekrtdobvqb:273bf4f1340c9a3c15afecc3c58690eda6e3bc1f29fd1a29e7ecdf949d693633@ec2-54-75-229-201.eu-west-1.compute.amazonaws.com:5432/d28q1qpcbvrn73"
 
 const webConnection = new Sequelize(DATABASE_URL);
@@ -15,7 +13,7 @@ const localConnection = new Sequelize('chessdb', 'postgres', '1111', {
     dialect: 'postgres'
 });
 
-const connection = webConnection || localConnection;
+const connection = /*webConnection ||*/ localConnection;
 /*connection.sync({
     force: true
 });*/
